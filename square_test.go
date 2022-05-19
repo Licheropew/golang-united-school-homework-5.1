b755bf1ece77219c974af77b526863a1159920c5
unknown <anton.aksiutsin@gmail.com>
2022-05-19 22:37:19 +0300

    Change test file, change results and error texts

--- square_test.go
+++ square_test.go
@@ -4,32 +4,30 @@ import (
 	"testing"
 )
 
-const (
-	funcErr = "Real result not equal expected result."
-)
-
 func TestSquare(t *testing.T) {
 	p := Point{x: 0, y: 0}
 	s := Square{start: p, a: 4}
+	var perimeterResult uint = 16
+	var areaResult uint = 16
+	endPointResult := Point{x: 4, y: 4}
 	t.Run("Area", func(t *testing.T) {
 		funcResult := s.Area()
-		result := s.a * s.a
-		if funcResult != result {
-			t.Errorf("%s %d != %d\n", funcErr, funcResult, result)
+		if funcResult != areaResult {
+			t.Errorf("square area is not correct. Got: %d. Expect: %d.\n", funcResult, areaResult)
 		}
 	})
 	t.Run("Perimeter", func(t *testing.T) {
 		funcResult := s.Perimeter()
-		result := 4 * s.a
-		if funcResult != result {
-			t.Errorf("%s %d != %d\n", funcErr, funcResult, result)
+
+		if funcResult != perimeterResult {
+			t.Errorf("square perimeter is not correct. Got: %d. Expect: %d.\n", funcResult, perimeterResult)
 		}
 	})
 	t.Run("End Point", func(t *testing.T) {
 		funcResult := s.End()
-		result := Point{x: s.start.x + int(s.a), y: s.start.y + int(s.a)}
-		if funcResult != result {
-			t.Errorf("%s %d != %d\n", funcErr, funcResult, result)
+
+		if funcResult != endPointResult {
+			t.Errorf("square end point is not correct. Got: %d. Expect: %d.\n", funcResult, endPointResult)
 		}
 	})
 }
