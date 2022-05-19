f78bbe03f649d9c2dcb98d3476e9232769a3f158
unknown <anton.aksiutsin@gmail.com>
2022-05-19 21:58:40 +0300

    Change test file, remove text to const

--- square_test.go
+++ square_test.go
@@ -4,6 +4,10 @@ import (
 	"testing"
 )
 
+const (
+	funcErr = "Real result not equal expected result."
+)
+
 func TestSquare(t *testing.T) {
 	p := Point{x: 0, y: 0}
 	s := Square{start: p, a: 4}
@@ -11,21 +15,21 @@ func TestSquare(t *testing.T) {
 		funcResult := s.Area()
 		result := s.a * s.a
 		if funcResult != result {
-			t.Errorf("Real result not equal expected result. %d != %d\n", funcResult, result)
+			t.Errorf("%s %d != %d\n", funcErr, funcResult, result)
 		}
 	})
 	t.Run("Perimeter", func(t *testing.T) {
 		funcResult := s.Perimeter()
 		result := 4 * s.a
 		if funcResult != result {
-			t.Errorf("Real result not equal expected result. %d != %d\n", funcResult, result)
+			t.Errorf("%s %d != %d\n", funcErr, funcResult, result)
 		}
 	})
 	t.Run("End Point", func(t *testing.T) {
 		funcResult := s.End()
 		result := Point{x: s.start.x + int(s.a), y: s.start.y + int(s.a)}
 		if funcResult != result {
-			t.Errorf("Real result not equal expected result. %d != %d\n", funcResult, result)
+			t.Errorf("%s %d != %d\n", funcErr, funcResult, result)
 		}
 	})
 }
